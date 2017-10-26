30b02a9852075d4dc11ed86ee1cfc40a77a9c742
Michelle Cinello <m.cinello@gmail.com>
2017-10-26 12:12:21 -0400

    deleted skips in methods

--- test_robot.rb
+++ test_robot.rb
@@ -5,7 +5,6 @@ require './robot.rb'
 class TestRobot < MiniTest::Test
 
   def test_that_foreign_robot_needing_repairs_sent_to_station_1
-    skip
     # arrange
 
     # act
@@ -14,7 +13,6 @@ class TestRobot < MiniTest::Test
   end
 
   def test_that_vintage_robot_needing_repairs_sent_to_station_2
-    skip
     # arrange
 
     # act
@@ -23,7 +21,6 @@ class TestRobot < MiniTest::Test
   end
 
   def test_that_standard_robot_needing_repairs_sent_to_station_3
-    skip
     # arrange
 
     # act
@@ -32,7 +29,6 @@ class TestRobot < MiniTest::Test
   end
 
   def test_that_robot_in_good_condition_sent_to_station_4
-    skip
     # arrange
 
     # act
@@ -41,7 +37,6 @@ class TestRobot < MiniTest::Test
   end
 
   def test_prioritize_tasks_with_empty_todo_list_returns_negative_one
-    skip
     # arrange
 
     # act
@@ -50,7 +45,6 @@ class TestRobot < MiniTest::Test
   end
 
   def test_prioritize_tasks_with_todos_returns_max_todo_value
-    skip
     # arrange
 
     # act
@@ -59,7 +53,6 @@ class TestRobot < MiniTest::Test
   end
 
   def test_workday_on_day_off_returns_false
-    skip
     # arrange
 
     # act
@@ -68,7 +61,6 @@ class TestRobot < MiniTest::Test
   end
 
   def test_workday_not_day_off_returns_true
-    skip
     # arrange
 
     # act
